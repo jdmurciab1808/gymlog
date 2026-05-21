551a6f8ca29965ed591f948bd6bb8a3f9e5189a5
juan-murcia-jt <juan.murcia@jobandtalent.com>
2026-05-21 17:13:15 -0500

    Fix CSRF en Railway con SECURE_PROXY_SSL_HEADER

--- gymlog/settings.py
+++ gymlog/settings.py
@@ -94,3 +94,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
